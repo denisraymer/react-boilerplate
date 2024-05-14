012b586f54d4897706d3f9f0d2752533ac0831a7
egor.eichhorn <eichhornegor@gmail.com>
2024-05-15 00:58:22 +0600

    chore: Test pre commit

--- lint-staged.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    '*.{ts,tsx}': [
-        'eslint --fix',
-        'prettier --write',
-    ],
-};
--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { StyleProvider } from "./styles/StyleProvider/StyleProvider.tsx"
 function App() {
   return (
     <StyleProvider>
-                               <LocalizationContextProvider>
+                                  <LocalizationContextProvider>
         <AppRouter />
-      </LocalizationContextProvider>
+                              </LocalizationContextProvider>
     </StyleProvider>
   )
 }
