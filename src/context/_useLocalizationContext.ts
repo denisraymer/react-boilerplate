e2acf152ff7802cbff2b2724ce40c9ac5005aade
DenisRaymer <raymerdenis@gmail.com>
2024-05-19 00:36:55 +0600

    Adding git hooks (#1)
    fea: Updating and refining git hooks

--- src/context/_useLocalizationContext.ts
+++ src/context/_useLocalizationContext.ts
@@ -4,10 +4,11 @@ import { ILocale } from "@/types/_localization.types"
 import { ELang } from "@/types/_localization.types"
 
 export interface ILocalizationContextValue {
-    l: ILocale
-    setLocale: React.Dispatch<React.SetStateAction<ELang>>
+  l: ILocale
+  setLocale: React.Dispatch<React.SetStateAction<ELang>>
 }
 
 export const LocalizationContext = createContext({} as ILocalizationContextValue)
 
-export const useLocalizationContext = (): ILocalizationContextValue => useContext(LocalizationContext)
+export const useLocalizationContext = (): ILocalizationContextValue =>
+  useContext(LocalizationContext)
